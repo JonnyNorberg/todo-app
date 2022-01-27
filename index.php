61c9e7ff05519f8794c0e0ad6011670f27f51e4a
Jonny Norberg <90459832+JonnyNorberg@users.noreply.github.com>
2022-01-27 16:41:50 +0100

    Extra thruster added
    Added JS functionality

--- index.php
+++ index.php
@@ -81,5 +81,47 @@ require 'db.php';
         </div>
     </div>
     <!--Main content -->
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            $('.remove-to-do').click(function(){
+                const id = $(this).attr('id');
+                
+                $.post("app/remove.php", 
+                      {
+                          id: id
+                      },
+                      (data)  => {
+                         if(data){
+                             $(this).parent().hide(600);
+                         }
+                      }
+                );
+            });
+
+            $(".check-box").click(function(e){
+                const id = $(this).attr('data-todo-id');
+                
+                $.post('app/check.php', 
+                      {
+                          id: id
+                      },
+                      (data) => {
+                          if(data != 'error'){
+                              const h2 = $(this).next();
+                              if(data === '1'){
+                                  h2.removeClass('checked');
+                              }else {
+                                  h2.addClass('checked');
+                              }
+                          }
+                      }
+                );
+            });
+        });
+    </script>
+    
 </body>
 </html>
